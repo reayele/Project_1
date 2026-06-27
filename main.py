55ff97f749589118c191b8820958acf1f8e044e2
Rediet Ayele <reayele@gmail.com>
2026-06-27 19:31:45 -0400

    Complete UPC validation logic

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ upc = input("Enter a 12-digit UPC: ")
 
 
 first_11 = upc[:11]
-provided_check_digit = upc[-1]
+provided_check_digit = int(upc[-1])
 
 def find_UPC(first_11):
        print("The first 11 digits are  ", first_11)
@@ -20,9 +20,16 @@ def find_UPC(first_11):
        odd_1 = ((int(first_11[0])+ int(first_11[2])+ int(first_11[4])+ int(first_11[6])+ int(first_11[8])+ int(first_11[10]))* 3)
        even_1 = (int(first_11[1])+ int(first_11[3])+int(first_11[5])+int(first_11[7]) + int(first_11[9]))
        check_digits = (odd_1)+(even_1)
-       expected_check_digit = find_UPC(first_11)
        expected_check_digit = (10 - (check_digits % 10)) % 10
 
+       
        return expected_check_digit
-    
-    
+
+expected_check_digit = find_UPC(first_11)
+print("The expected check digit is ", expected_check_digit)
+
+if expected_check_digit == provided_check_digit:
+       print("This is a VALID UPC.")
+
+else:
+       print("This is an INVALID UPC.")
